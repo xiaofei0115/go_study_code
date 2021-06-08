61abb5598ba9a217b3bb0cb1ccebe7ce6c5ef80f
xiaofei0115 <xiaofei0115@sohu.com>
2021-06-08 19:05:55 +0800

    str study

--- const.go
+++ const.go
@@ -1,26 +1,58 @@
 package main
 
-import(
-	"io/ioutil"
+import (
 	"fmt"
+	"io/ioutil"
 )
 
-func bounded(v int) int{
-	if v>100{
+func bounded(v int) int {
+	if v > 100 {
 		return 100
-	}else if v<0{
+	} else if v < 0 {
 		return 0
-	}else{
+	} else {
 		return v
 	}
 }
-
-func main(){
-	const filename="abc.txt"
-	contents,err:=ioutil.ReadFile(filename)
-	if err !=nil{
-		fmt.Println(err)
-	}else{
-		fmt.Printf("%s\n",contents)
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "c"
+	case score < 90:
+		g = "b"
+	case score <= 100:
+		g = "A"
+	default:
+		panic(fmt.Sprintf("Wrong scroe %d", score)) //panic终止运行，并打印出错误
 	}
+	return g
+}
+
+func convertToBin(n int) string{
+	result:=""
+	for ;n>0;n/2={
+		lsb:=n%2
+		result=strconv.Itoa(lsb)+result
+	}
+	return result
+}
+
+
+func main() {
+	const filename = "abc.txt"
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+
+	fmt.Print(grade(200))
+	fmt.Println(
+		convertToBin(5)
+		convertToBin(13)
+	)
 }
